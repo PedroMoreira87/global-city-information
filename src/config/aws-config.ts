d88bad099b8986624e4f1de90a0cc6674a68544e
Pedro Henrique Moreira <pedrohmoreira87@gmail.com>
2025-10-09 17:51:05 -0300

    fix(domain): add a free domain

--- src/config/aws-config.ts
+++ src/config/aws-config.ts
@@ -3,6 +3,6 @@ export const awsConfig = {
   userPoolId: import.meta.env.VITE_USER_POOL_ID,
   userPoolWebClientId: import.meta.env.VITE_CLIENT_ID,
   oauthDomain: 'global-city-information.auth.us-east-1.amazoncognito.com',
-  redirectSignIn: 'https://globalcityinformation.org/',
-  redirectSignOut: 'https://globalcityinformation.org/',
+  redirectSignIn: 'https://d2u17pmw1fxael.cloudfront.net/',
+  redirectSignOut: 'https://d2u17pmw1fxael.cloudfront.net/',
 };
