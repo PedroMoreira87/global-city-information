e602e92b3c63af051d663ca12f69071235ff4e2b
Pedro Henrique Moreira <pedrohmoreira87@gmail.com>
2025-11-27 16:24:26 -0300

    fix(server-server-protection): add server to server api protection

--- src/apis/aws-lambda-functions.ts
+++ src/apis/aws-lambda-functions.ts
@@ -1,12 +1,25 @@
+import { fetchAuthSession } from "aws-amplify/auth";
 import axios from "axios";
 import type { IWeather } from "../interfaces/user.interface.ts";
 
+async function authHeaders() {
+	// Get current session and id token
+	const { tokens } = await fetchAuthSession();
+
+	if (!tokens?.idToken) {
+		throw new Error("No ID token available");
+	}
+
+	const token = tokens.idToken.toString();
+	return { Authorization: `Bearer ${token}` };
+}
+
 export const geolocation = async (cityInput: string) => {
+	const headers = await authHeaders();
 	const response = await axios.post(
 		"https://yzl2gp4vz5.execute-api.us-east-1.amazonaws.com/dev/geolocations",
-		{
-			cityInput,
-		},
+		{ cityInput },
+		{ headers },
 	);
 	return response.data;
 };
@@ -15,27 +28,30 @@ export const weatherData = async (
 	selectedCityLat: number,
 	selectedCityLon: number,
 ) => {
+	const headers = await authHeaders();
 	const response = await axios.post(
 		"https://yzl2gp4vz5.execute-api.us-east-1.amazonaws.com/dev/weather",
-		{
-			selectedCityLat,
-			selectedCityLon,
-		},
+		{ selectedCityLat, selectedCityLon },
+		{ headers },
 	);
 	return response.data;
 };
 
 export const updateUser = async (userId: string, userWeather: IWeather) => {
+	const headers = await authHeaders();
 	const response = await axios.put(
 		`https://yzl2gp4vz5.execute-api.us-east-1.amazonaws.com/dev/users/${userId}`,
 		userWeather,
+		{ headers },
 	);
 	return response.data;
 };
 
 export const getUser = async (userId: string) => {
+	const headers = await authHeaders();
 	const response = await axios.get(
 		`https://yzl2gp4vz5.execute-api.us-east-1.amazonaws.com/dev/users/${userId}`,
+		{ headers },
 	);
 	return response.data;
 };
